152af64f57db7384cb1026b458678ade483ed8a2
Larry <gravitywolfnotamused@gmail.com>
2021-01-07 20:19:51 -0700

    Update init.c

--- init.c
+++ init.c
@@ -1,3 +1,44 @@
+void main()
+{
+	//INIT WEATHER BEFORE ECONOMY INIT------------------------
+	Weather weather = g_Game.GetWeather();
+
+	weather.MissionWeather(false);    // false = use weather controller from Weather.c
+
+	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.4, 0.6), 1, 0);
+	weather.GetRain().Set( 0, 0, 1);
+	weather.GetFog().Set( Math.RandomFloatInclusive(0.05, 0.1), 1, 0);
+
+	//INIT ECONOMY--------------------------------------
+	//Hive ce = CreateHive();
+	//if ( ce )
+	//	ce.InitOffline();
+
+      
+	//DATE RESET AFTER ECONOMY INIT-------------------------
+	int year, month, day, hour, minute;
+	int reset_month = 9, reset_day = 20;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+
+    if ((month == reset_month) && (day < reset_day))
+    {
+        GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+    }
+    else
+    {
+        if ((month == reset_month + 1) && (day > reset_day))
+        {
+            GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+        }
+        else
+        {
+            if ((month < reset_month) || (month > reset_month + 1))
+            {
+                GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+            }
+        }
+    }
+}
 
 class CustomMission : MissionServer
 {
@@ -299,4 +340,9 @@ class CustomMission : MissionServer
 	{
 		Print("[SpawnLoadout]:: " + msg);
 	}
+}
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
 }
