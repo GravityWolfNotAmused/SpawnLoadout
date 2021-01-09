d97f80150a7f96b775f2359174414675eefff70a
Larry <gravitywolfnotamused@gmail.com>
2021-01-09 12:53:28 -0700

    Update init.c

--- init.c
+++ init.c
@@ -10,9 +10,9 @@ void main()
 	weather.GetFog().Set( Math.RandomFloatInclusive(0.05, 0.1), 1, 0);
 
 	//INIT ECONOMY--------------------------------------
-	//Hive ce = CreateHive();
-	//if ( ce )
-	//	ce.InitOffline();
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
 
       
 	//DATE RESET AFTER ECONOMY INIT-------------------------
